6be499709b731d13c3f54256b226cbc66227a823
Cutehackerboy <143728551+Cutehackerboy@users.noreply.github.com>
2023-09-05 18:11:54 +0530

    Create Setup.bash

---
+++ Setup.bash
@@ -0,0 +1,22 @@
+Echo Please wait this Process take 2-3 min
+pkg update 
+pkg upgrade
+apt update
+clear
+pkg install git
+pkg install cowsay
+pkg install toilet
+pkg install wgit
+clear
+toilet Successfully 
+echo .
+echo .
+clear
+cowsay Successfully completed 
+echo .
+echo.
+echo.
+clear
+echo now run track.bash using bash track.bash..
+
+toilet Successfully
