c715666d5d699a27e31e23b9eeab32b8aaf5f63c
Cutehackerboy <143728551+Cutehackerboy@users.noreply.github.com>
2023-09-06 12:26:54 +0530

    Update trackimei.py

--- trackimei.py
+++ trackimei.py
@@ -11,7 +11,7 @@ for i in range(1, rows + 1):
     for j in range(1, i + 1):
         print("*", end=" ")
     print()
-  print("Finding number)
+  print("Finding number")
         rows = 5  # You can change the number of rows as needed
 
 for i in range(1, rows + 1):
