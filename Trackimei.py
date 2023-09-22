001d3ce1b88c169d0e4cec33c3ece4f2b9acd0d7
Cutehackerboy <143728551+Cutehackerboy@users.noreply.github.com>
2023-09-22 15:58:29 +0530

    Update Trackimei.py

--- Trackimei.py
+++ Trackimei.py
@@ -1,26 +1,21 @@
-print("Hi This is a Imei Track Pakeage let Start but must run Setup.sh file ok")
-# Ask the user for their name
+user_name = input("Enter Your imei to track? ")pkg install python
+print("Hello user This is new Version of Imei track it could not working properly we fix it soon")
+
 user_name = input("Enter Your imei to track? ")
-
-# Print a greeting using the user's name
+print("Try to find imei")
 print(f"Imei, {user_name}!")
-print("Loading....")
-rows = 5  # You can change the number of rows as needed
+print ("Tracking...")
+Clear
+print(f"Imei, {user_name}!")
+print("Found")
+print("Valid that this is your phone")
+user_id = input("Enter This imei phone name")
 
-for i in range(1, rows + 1):
-    for j in range(1, i + 1):
-        print("*", end=" ")
-    print()
-        rows = 5  # You can change the number of rows as needed
-
-for i in range(1, rows + 1):
-    for j in range(1, i + 1):
-        print("*", end=" ")
-    print()
-  
-  import random
-
+print("Successfully")
+print(f"Imei, {user_name}!")
+print(f"Name, {user_id}!")
 # Generate a random 10-digit number
 random_number = random.randint(10**9, (10**10)-1)
+print("Number")
 print(random_number)
 print("Successfully")￼Enter
