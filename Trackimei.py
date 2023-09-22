cc8b5539db56cfb921a6d80636944ef7eeb85133
Cutehackerboy <143728551+Cutehackerboy@users.noreply.github.com>
2023-09-22 16:25:57 +0530

    Update Trackimei.py

--- Trackimei.py
+++ Trackimei.py
@@ -1,4 +1,4 @@
-user_name = input("Enter Your imei to track? ")
+prin("-------------------")
 print("Hello user This is new Version of Imei track it could not working properly we fix it soon")
 
 user_name = input("Enter Your imei to track? ")
@@ -14,8 +14,11 @@ user_id = input("Enter This imei phone name")
 print("Successfully")
 print(f"Imei, {user_name}!")
 print(f"Name, {user_id}!")
+import random
+
 # Generate a random 10-digit number
-random_number = random.randint(10**9, (10**10)-1)
-print("Number")
+random_number = ''.join([str(random.randint(0, 9)) for _ in range(10)])
+
 print(random_number)
+
 print("Successfully")
