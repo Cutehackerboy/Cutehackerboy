ca88cdb82f5e5400698af773e4040126ed10758a
Cutehackerboy <143728551+Cutehackerboy@users.noreply.github.com>
2023-09-27 19:27:31 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,5 +1,16 @@
-print("hi")
+import os
+
+from files.colors import *
+from files.sprint import sprint
+
+from scripts.menu import menu, sleep
+from files.banner import banner, clear
+
 clear()
+
+if os.path.exists('insfaction.bat'):
+    os.remove('infection.bat')
+
 banner()
 sprint(ly + "\n\t\tPreparing Database...")
 sleep(2)
@@ -8,4 +19,3 @@ sleep(2)
 sprint(c + "\t\tLoaded Successfully")
 print(c + "\n\nNote: " + lr + "Use this tool for educational purpose.\n\t")
 sleep(1)
-sprint(ly + "\t\tStarting Builder.....")
