3dce279545e31598a905aaecd3a77923f89f4012
Cutehackerboy <143728551+Cutehackerboy@users.noreply.github.com>
2023-09-27 18:56:06 +0530

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,2 +1,18 @@
-from termcolor import colored
-print(colored('hii','blue')
+import os
+
+from files.colors import *
+from files.sprint import sprint
+
+from scripts.menu import menu, sleep
+from files.banner import banner, clear
+
+clear()
+banner()
+sprint(ly + "\n\t\tPreparing Database...")
+sleep(2)
+sprint(ly + "\n\n\t\tAlmost Done...")
+sleep(2)
+sprint(c + "\t\tLoaded Successfully")
+print(c + "\n\nNote: " + lr + "Use this tool for educational purpose.\n\t")
+sleep(1)
+sprint(ly + "\t\tStarting Builder.....")
