beea2e517399aac2df87d99d5d3c7215770198b8
Cutehackerboy <143728551+Cutehackerboy@users.noreply.github.com>
2023-10-05 19:18:14 +0530

    Update Setup.bash

--- Setup.bash
+++ Setup.bash
@@ -1,3 +1,22 @@
+clear
+center() {
+  termwidth=$(stty size | cut -d" " -f2)
+  padding="$(printf '%0.1s' ={1..500})"
+  printf '%*.*s %s %*.*s\n' 0 "$(((termwidth-2-${#1})/2))" "$padding" "$1" 0 "$(((termwidth-1-${#1})/2))" "$padding"
+}
+grn="\e[92m"
+cod="\e[93m"
+echo -e '\e[92m
+           🅷🅸 🅸🅼🅴🅸 🆃🆁🅰🅲🅺 
+echo -e "\e[92m"
+center "SETUP PROCESS"
+echo -e "
+       $grn-----------------------------------$grn
+      |$grn FOLLLOW:$cod  Cutehackerboy $grn |$grn
+      |$grn Mod:$cod 1.19          $grn |$grn
+      |$grn Wellcome To Imei Tarck:$       $grn |$grn
+       $grn-----------------------------------$grn
+"
 echo Please wait this Process take 2-3 min
 pkg update 
 pkg upgrade
